35af4cad55a4fb0627c228f5e7d4a22fdc881e42
JonathanPicques <jonathan.picques@gmail.com>
2024-10-08 17:20:31 +0200

    ux: use titles instead of aria for icon buttons

--- src/routes/widgets/GithubLink.svelte
+++ src/routes/widgets/GithubLink.svelte
@@ -2,6 +2,6 @@
     import Github from '$lib/core/widgets/icons/Github.svelte';
 </script>
 
-<a href="https://github.com/JonathanPicques/exeflow" class="icon button" target="_blank" aria-label="Exeflow Github repo">
+<a href="https://github.com/JonathanPicques/exeflow" class="icon button" title="Exeflow Github repo" target="_blank">
     <Github size="2rem" />
 </a>
--- src/routes/widgets/ProfileLink.svelte
+++ src/routes/widgets/ProfileLink.svelte
@@ -2,6 +2,6 @@
     import Avatar from '$lib/core/widgets/icons/Avatar.svelte';
 </script>
 
-<a href="/auth/profile" class="icon button" aria-label="Your profile">
+<a href="/auth/profile" class="icon button" title="Your profile">
     <Avatar size="2rem" />
 </a>
