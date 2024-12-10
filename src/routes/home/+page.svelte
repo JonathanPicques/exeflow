8918f3785911566386107204dba71d4fb1e33e0f
JonathanPicques <jonathan.picques@gmail.com>
2024-12-10 22:37:53 +0100

    feat: improve home page

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
     import Fuse from 'fuse.js';
     import moment from 'moment';
+    import {goto} from '$app/navigation';
 
     import empty from './+empty.svg';
 
     import Logo from '$lib/core/widgets/Logo.svelte';
+    import Trash from '$lib/core/widgets/icons/Trash.svelte';
     import Navbar from '$lib/core/widgets/Navbar.svelte';
     import GithubLink from '$lib/core/widgets/GithubLink.svelte';
     import ProfileLink from '$lib/core/widgets/ProfileLink.svelte';
 
+    import {wait} from '$lib/core/helper/function';
     import {postProject, deleteProject} from '../api/project/project';
     import type {Project} from '../api/project/project';
 
@@ -16,21 +19,34 @@
     let filter = $state('');
     let projects: Project[] = $state(data.projects);
 
+    let creating = $state(false);
     const filteredProjects = $derived.by(() => {
         if (filter === '') return projects;
 
-        const fuse = new Fuse(projects, {keys: ['name']});
+        const fuse = new Fuse(projects, {keys: ['name'], shouldSort: false});
         const found = fuse.search(filter);
         return found.map(f => f.item);
     });
 
     const createProject = async () => {
-        projects = [await postProject({name: `Untitled project ${projects.length}`}), ...projects];
+        creating = true;
+        Promise.all([postProject({name: `Untitled project ${projects.length}`}), wait(500)])
+            .then(([project]) => {
+                return goto(`/project/${project.id}`);
+            })
+            .then(() => {
+                creating = false;
+            })
+            .catch(() => {
+                creating = false;
+            });
     };
 
-    const removeProject = async (id: string) => {
-        await deleteProject({id});
-        projects = projects.filter(p => p.id !== id);
+    const removeProject = async (project: Project) => {
+        if (confirm(`Are you sure you want to remove ${project.name}?`)) {
+            await deleteProject({id: project.id});
+            projects = projects.filter(p => p.id !== project.id);
+        }
     };
 </script>
 
@@ -42,19 +58,17 @@
     {#snippet left()}
         <Logo />
     {/snippet}
-    {#snippet center()}
-        <input type="search" placeholder="Filter projects..." bind:value={filter} />
-    {/snippet}
     {#snippet right()}
-        <button onclick={createProject}>+ New project</button>
-        <!-- <a href="/auth/profile">Profile</a> -->
-        <!-- <a href="/auth/logout" data-sveltekit-reload>Logout</a> -->
         <ProfileLink />
         <GithubLink />
     {/snippet}
 </Navbar>
 
 <main>
+    <div class="toolbar">
+        <input type="search" placeholder="Filter projects..." bind:value={filter} />
+        <button disabled={creating} onclick={createProject}>New automation project</button>
+    </div>
     <div class="projects">
         {#each filteredProjects as project}
             <div class="project">
@@ -66,7 +80,9 @@
                         <span class="title">{project.name}</span>
                         <span class="update">Updated {moment(project.updated_at).fromNow()}</span>
                     </div>
-                    <button class="custom" onclick={() => removeProject(project.id)}>❌</button>
+                    <button class="icon" onclick={() => removeProject(project)}>
+                        <Trash />
+                    </button>
                 </div>
             </div>
         {/each}
@@ -75,15 +91,13 @@
 
 <style>
     main {
+        gap: 1rem;
         display: flex;
         overflow: auto;
         flex-grow: 1;
+        margin-top: 6rem;
         padding-inline: 3rem;
         flex-direction: column;
-    }
-
-    input {
-        width: clamp(10rem, 30%, 40rem);
     }
 
     .project {
@@ -94,7 +108,7 @@
 
             background-color: var(--color-bg-1);
 
-            transition: 0.3s;
+            transition: border 0.3s;
 
             &:hover {
                 border: 0.15rem solid var(--color-fg);
@@ -121,20 +135,20 @@
                     font-size: 0.8rem;
                 }
             }
-
-            & > button {
-                margin: 0;
-                padding: 0;
-                background-color: transparent;
-            }
         }
     }
 
+    .toolbar {
+        gap: 1rem;
+        display: flex;
+    }
+
     .projects {
-        margin-top: 6rem;
         display: grid;
         grid-gap: 2rem;
+        overflow: auto;
         flex-grow: 1;
+        padding-right: 1rem;
         align-content: start;
         grid-template-columns: repeat(auto-fill, minmax(clamp(15rem, 25vw, 25rem), 1fr));
     }
