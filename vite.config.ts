edb1925e91c12180038f759dc9a4ffd4338be0d8
JonathanPicques <jonathan.picques@gmail.com>
2025-02-18 21:18:20 +0100

    feat: disable plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,45 @@
 import {sveltekit} from '@sveltejs/kit/vite';
-import {defineConfig} from 'vitest/config';
+import {loadEnv, defineConfig} from 'vite';
 
 import {sveltekitOpenapi} from './vite.plugins';
 
-export default defineConfig({
-    test: {
-        watch: false,
-        include: ['src/**/*.{test,spec}.{js,ts}'],
-        typecheck: {
-            enabled: true,
-            ignoreSourceErrors: true,
-        },
-    },
-    plugins: [
-        sveltekit() as any, // TODO: Remove as any when fixed
-        sveltekitOpenapi({
-            openapi: {
-                info: {
-                    title: 'Exeflow API',
-                    version: '0.1.0',
-                    description: 'API documentation for the Exeflow project',
-                },
-                servers: [{url: 'http://localhost:5173'}, {url: 'https://exeflow.vercel.app/'}],
-                components: {
-                    securitySchemes: {
-                        cookieAuth: {
-                            in: 'cookie',
-                            type: 'apiKey',
-                            name: 'exeflow_session',
-                        },
-                    },
-                },
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    const disableOpenapiVitePlugin = env.EXEFLOW_DISABLE_OPENAPI_VITE_PLUGIN === 'true';
+
+    return {
+        test: {
+            watch: false,
+            include: ['src/**/*.{test,spec}.{js,ts}'],
+            typecheck: {
+                enabled: true,
+                ignoreSourceErrors: true,
             },
-            include: ['**/*.ts', '!**/*.test*.ts', '!**/*.svelte*.ts'],
-        }),
-    ],
+        },
+        plugins: [
+            sveltekit(),
+            disableOpenapiVitePlugin
+                ? undefined
+                : sveltekitOpenapi({
+                      openapi: {
+                          info: {
+                              title: 'Exeflow API',
+                              version: '0.1.0',
+                              description: 'API documentation for the Exeflow project',
+                          },
+                          servers: [{url: 'http://localhost:5173'}, {url: 'https://exeflow.vercel.app/'}],
+                          components: {
+                              securitySchemes: {
+                                  cookieAuth: {
+                                      in: 'cookie',
+                                      type: 'apiKey',
+                                      name: 'exeflow_session',
+                                  },
+                              },
+                          },
+                      },
+                      include: ['**/*.ts', '!**/*.test*.ts', '!**/*.svelte*.ts'],
+                  }),
+        ],
+    };
 });
